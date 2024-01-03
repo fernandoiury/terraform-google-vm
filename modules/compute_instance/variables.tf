4e0aae4759d6a85acb9b2a64c724f974c2560606
fernando.costa <fernandoiury@gmail.com>
2024-01-03 13:57:17 -0500

    feat: Adds support for additional networks in compute_instance

--- modules/compute_instance/variables.tf
+++ modules/compute_instance/variables.tf
@@ -128,3 +128,40 @@ variable "resource_manager_tags" {
   type        = map(string)
   default     = null
 }
+
+variable "additional_networks" {
+  description = "Additional network interface details for GCE, if any."
+  default     = []
+  type = list(object({
+    network            = string
+    subnetwork         = string
+    subnetwork_project = string
+    network_ip         = string
+    nic_type           = string
+    stack_type         = string
+    queue_count        = number
+    access_config = list(object({
+      nat_ip       = string
+      network_tier = string
+    }))
+    ipv6_access_config = list(object({
+      network_tier = string
+    }))
+    alias_ip_range = list(object({
+      ip_cidr_range         = string
+      subnetwork_range_name = string
+    }))
+  }))
+  validation {
+    condition = alltrue([
+      for ni in var.additional_networks : ni.nic_type == "GVNIC" || ni.nic_type == "VIRTIO_NET" || ni.nic_type == null
+    ])
+    error_message = "In the variable additional_networks, field \"nic_type\" must be either \"GVNIC\", \"VIRTIO_NET\" or null."
+  }
+  validation {
+    condition = alltrue([
+      for ni in var.additional_networks : ni.stack_type == "IPV4_ONLY" || ni.stack_type == "IPV4_IPV6" || ni.stack_type == null
+    ])
+    error_message = "In the variable additional_networks, field \"stack_type\" must be either \"IPV4_ONLY\", \"IPV4_IPV6\" or null."
+  }
+}
